d036775f59ca288f5248e39632f7d84f252efca2
omutucat <57890334+omutucat@users.noreply.github.com>
2021-08-19 20:45:47 +0900

    rewriteshite

---
+++ RhythmConnect/Assets/Scripts/General/SystemConfigManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemConfigManager : GeneralFileReader
+{
+    public char[] KeyConfig { get; private set; } = new char[4];
+    public string SongDataFolderPath { get; private set; }
+
+    public bool AnalyseConfigString(string filename)
+    {
+        LoadFile(filename);
+
+        foreach (string line in Line)
+        {
+            string[] temp = line.Split(' ');
+
+            switch (temp[0])
+            {
+                case "#KEYCONFIG":
+                    string[] kc = temp[1].Split(',');
+                    for (int j = 0; j < 4; j++)
+                    {
+                        KeyConfig[j] = kc[j][0];
+                    }
+                    break;
+
+                case "#SONGDATAFOLDER":
+                    SongDataFolderPath = temp[1];
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnalyseConfigString("systemconfig.rccfg");
+        Debug.Log(SongDataFolderPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
