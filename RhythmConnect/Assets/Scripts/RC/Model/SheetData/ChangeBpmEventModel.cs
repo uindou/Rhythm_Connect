d1c0d9fcddc0f9fcd40249dac29abb327f0d3666
omutucat <57890334+omutucat@users.noreply.github.com>
2023-07-11 23:22:11 +0900

    譜面データモデル群の追加

---
+++ RhythmConnect/Assets/Scripts/RC/Model/SheetData/ChangeBpmEventModel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RC.Model
+{
+    public class ChangeBpmEventModel : NoteModel
+    {
+        public double Bpm { get; private set; }
+
+        public ChangeBpmEventModel(int count, double bpm)
+        {
+            Count = count;
+            Bpm = bpm;
+        }
+    }
+}
