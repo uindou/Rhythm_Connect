c4fb01df8071d81858ea8e1dd87f702b5b61026a
omutucat <57890334+omutucat@users.noreply.github.com>
2023-07-11 23:21:21 +0900

    Constクラスの追加

---
+++ RhythmConnect/Assets/Scripts/RC/Const/GameConst.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RC.Const
+{
+    public enum Difficulty
+    {
+        Simple
+      , Decent
+      , Complex
+    }
+
+    public enum NoteType
+    {
+        None
+      , Tap
+      , StartHold
+      , EndHold
+    }
+
+    public enum Channels
+    {
+        LaneA
+      , LaneB
+      , LaneC
+      , LaneD
+      , LaneAB
+      , LaneBC
+      , LaneCD
+      , LaneABC
+      , LaneBCD
+      , LaneABCD
+      , ChangeBpm
+      , Bar
+    }
+
+    public class GameConst
+    {
+        public static readonly int BarResolution = 9600;
+    }
+}
